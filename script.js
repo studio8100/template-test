8ca99e17caf1fc12a09dc8eee59238a30e2b2f22
Paul Carstens <studio8100@yahoo.com>
2020-07-24 12:01:03 -0400

    Added heroku self server to pull in quote API request without issues

---
+++ script.js
@@ -0,0 +1,24 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterButton = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+// Get Quote from API
+async function getQuote() {
+    const proxyUrl = 'https://infinite-anchorage-92673.herokuapp.com/';
+    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+    try{
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        authorText.innerText = data.quoteAuthor;
+        quoteText.innerText = data.quoteText
+
+    } catch(error) {
+    }
+}
+
+// On Load
+getQuote();
